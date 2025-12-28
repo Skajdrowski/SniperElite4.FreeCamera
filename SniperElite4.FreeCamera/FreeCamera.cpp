8e62b8b0b5f7c79f57dac8e50fed94c386033cd9
Skajdrovski <cssteam1337pl@gmail.com>
2025-12-28 23:48:52 +0100

    Fix crash on startup

--- SniperElite4.FreeCamera/FreeCamera.cpp
+++ SniperElite4.FreeCamera/FreeCamera.cpp
@@ -3,7 +3,9 @@
 #include "se4/Camera.h"
 #include "SettingsMgr.h"
 
+Camera* FreeCamera::cam = nullptr;
 unsigned int FreeCamera::ms_bEnabled = 0;
+uintptr_t FreeCamera::CordsInstruction = 0;
 
 void FreeCamera::Init()
 {
@@ -12,9 +14,6 @@ void FreeCamera::Init()
 
 void FreeCamera::Thread()
 {
-	uintptr_t CordsInstruction = SigScan("89 ? 49 89 ? 8B ? ? 89 ? ? 8B ? ? 89 ? ? 8B ? ? 89 ? ? 8B ? ? 89 ? ? 8B ? ? 89 ? ? 8B ? ? 89 ? ? 8B ? ? 89 ? ? 8B ? ? 89 ? ? 8B ? ? 89 ? ? 8B ? ? 89 ? ? 8B ? ? 89 ? ? F2");
-	Camera* cam = GetCamera();
-
 	while (true)
 	{
 		if (GetAsyncKeyState(SettingsMgr->iFreeCameraEnableKey) & 0x1)
@@ -22,6 +21,12 @@ void FreeCamera::Thread()
 
 		if (ms_bEnabled == 1)
 		{
+			if (!cam)
+				cam = GetCamera();
+
+			if (!CordsInstruction)
+				CordsInstruction = SigScan("89 ? 49 89 ? 8B ? ? 89 ? ? 8B ? ? 89 ? ? 8B ? ? 89 ? ? 8B ? ? 89 ? ? 8B ? ? 89 ? ? 8B ? ? 89 ? ? 8B ? ? 89 ? ? 8B ? ? 89 ? ? 8B ? ? 89 ? ? 8B ? ? 89 ? ? 8B ? ? 89 ? ? F2");
+
 			Nop(CordsInstruction, 2);
 			Nop(CordsInstruction + 8, 3);
 			Nop(CordsInstruction + 14, 3);
@@ -29,34 +34,31 @@ void FreeCamera::Thread()
 		}
 		else if (ms_bEnabled == 2)
 		{
-			if (cam)
-			{
-				float speed = SettingsMgr->fFreeCameraSpeed;
+			float speed = SettingsMgr->fFreeCameraSpeed;
 
-				if (GetAsyncKeyState(SettingsMgr->iFreeCameraKeySlowDown))
-					speed /= SettingsMgr->fFreeCameraModifierScale;
+			if (GetAsyncKeyState(SettingsMgr->iFreeCameraKeySlowDown))
+				speed /= SettingsMgr->fFreeCameraModifierScale;
 
-				if (GetAsyncKeyState(SettingsMgr->iFreeCameraKeySpeedUp))
-					speed *= SettingsMgr->fFreeCameraModifierScale;
+			if (GetAsyncKeyState(SettingsMgr->iFreeCameraKeySpeedUp))
+				speed *= SettingsMgr->fFreeCameraModifierScale;
 
-				Vector fwd = cam->Rotation.GetForward();
-				Vector up = cam->Rotation.GetUp();
-				Vector right = cam->Rotation.GetRight();
-				if (GetAsyncKeyState(SettingsMgr->iFreeCameraKeyForward))
-					cam->Position += fwd * speed * 1;
-				if (GetAsyncKeyState(SettingsMgr->iFreeCameraKeyBack))
-					cam->Position += fwd * speed * -1;
+			Vector fwd = cam->Rotation.GetForward();
+			Vector up = cam->Rotation.GetUp();
+			Vector right = cam->Rotation.GetRight();
+			if (GetAsyncKeyState(SettingsMgr->iFreeCameraKeyForward))
+				cam->Position += fwd * speed * 1;
+			if (GetAsyncKeyState(SettingsMgr->iFreeCameraKeyBack))
+				cam->Position += fwd * speed * -1;
 
-				if (GetAsyncKeyState(SettingsMgr->iFreeCameraKeyUp))
-					cam->Position += up * speed * 1;
-				if (GetAsyncKeyState(SettingsMgr->iFreeCameraKeyDown))
-					cam->Position += up * speed * -1;
+			if (GetAsyncKeyState(SettingsMgr->iFreeCameraKeyUp))
+				cam->Position += up * speed * 1;
+			if (GetAsyncKeyState(SettingsMgr->iFreeCameraKeyDown))
+				cam->Position += up * speed * -1;
 
-				if (GetAsyncKeyState(SettingsMgr->iFreeCameraKeyRight))
-					cam->Position += right * speed * 1;
-				if (GetAsyncKeyState(SettingsMgr->iFreeCameraKeyLeft))
-					cam->Position += right * speed * -1;
-			}
+			if (GetAsyncKeyState(SettingsMgr->iFreeCameraKeyRight))
+				cam->Position += right * speed * 1;
+			if (GetAsyncKeyState(SettingsMgr->iFreeCameraKeyLeft))
+				cam->Position += right * speed * -1;
 		}
 		else if (ms_bEnabled == 3)
 		{
--- SniperElite4.FreeCamera/FreeCamera.h
+++ SniperElite4.FreeCamera/FreeCamera.h
@@ -1,10 +1,13 @@
 #pragma once
 
+struct Camera;
+
 class FreeCamera {
 public:
-	static unsigned int ms_bEnabled;
-
 	static void Init();
 	static void Thread();
-
+private:
+	static Camera* cam;
+	static unsigned int ms_bEnabled;
+	static uintptr_t CordsInstruction;
 };
