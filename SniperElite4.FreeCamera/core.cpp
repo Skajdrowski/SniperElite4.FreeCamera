1d0e93b7ba7759449ce4cef1484b2b8901729887
Skajdrovski <cssteam1337pl@gmail.com>
2025-12-28 19:44:52 +0100

    Support for both DX11 & DX12

--- SniperElite4.FreeCamera/core.cpp
+++ SniperElite4.FreeCamera/core.cpp
@@ -10,3 +10,18 @@ uintptr_t _addr(uintptr_t addr)
 {
 	return GetEntryPoint() + addr;
 }
+
+uintptr_t SigScan(const char* pattern, uint32_t pad, uint32_t lenght)
+{
+	hook::pattern Pattern(pattern);
+	uintptr_t OpcodeAddr = Pattern.count(1).get(0).get_uintptr(pad);
+
+	if (lenght != 0)
+	{
+		int32_t RIPOffset;
+		Read(OpcodeAddr, RIPOffset);
+		uintptr_t Addr = OpcodeAddr + lenght + RIPOffset;
+		return Addr;
+	}
+	return OpcodeAddr;
+}
